5ac54d70707d5397e808fb08b2d674ecd76791b5
princelailan <jesuspromisesmedia@gmail.com>
2025-04-16 19:28:32 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from pet import Pet
+
+chippy = Pet("Chippy")
+
+print("Creating pet: Chippy the Squirrel\n")
+
+chippy.eat()
+chippy.play()
+chippy.collect_acorns()
+chippy.sleep()
+chippy.groom()
+chippy.train("do a backflip")
+chippy.train("hide acorns")
+chippy.get_status()
+chippy.show_tricks()
